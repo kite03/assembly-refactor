218cb4e604535d66b46cc656c6777cd5ede2b616
Targeted Individual <67329371+kite03@users.noreply.github.com>
2024-03-21 13:55:56 +0000

    Initial Commit

---
+++ AssemblyRefactor/Program.cs
@@ -0,0 +1,31 @@
+﻿using dnlib.DotNet;
+
+namespace AssemblyRefactor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Provide the path to the assembly to refactor");
+                return;
+            }
+
+            string inputFilePath = args[0];
+
+            ModuleDefMD module = ModuleDefMD.Load(inputFilePath);
+            Refactor.RefactorAssembly(module);
+
+            string directory = Path.GetDirectoryName(inputFilePath);
+            string filename = Path.GetFileNameWithoutExtension(inputFilePath);
+            string extension = Path.GetExtension(inputFilePath);
+
+            string output = Path.Combine(directory, $"{filename}_refactored{extension}");
+
+            Console.WriteLine($"Constructing new assembly at: {output}");
+            module.Write(output);
+            Console.WriteLine($"Done!");
+        }
+    }
+}
---
+++ AssemblyRefactor/Refactor.cs
@@ -0,0 +1,87 @@
+﻿using dnlib.DotNet;
+using System.Text.RegularExpressions;
+
+namespace AssemblyRefactor
+{
+    internal class Refactor
+    {
+        // whatever method you use to detect obfuscated names
+        private static bool ShouldRename(string name)
+        {
+            // Generic A-Z Detection
+            Regex regex = new Regex(@"[a-zA-Z0-9_.]+");
+            return !regex.IsMatch(name);
+
+            // VRChat
+            // return name.Any(c => "ÌÍÎÏ".Contains(c));
+
+            // Rename everything
+            // return true;
+        }
+
+        private static void ProcessParameter(ParamDef parameter, ref int count)
+        {
+            if (ShouldRename(parameter.Name))
+            {
+                parameter.Name = $"p{count++}";
+            }
+        }
+
+        private static void RenameEntity(IMemberDef entity, ref int count)
+        {
+            if (!ShouldRename(entity.Name)) 
+            {
+                Console.WriteLine($"Skipping {entity.Name}");
+                return;
+            }
+
+            Console.WriteLine(entity.GetType().Name);
+
+            string typeName = entity.GetType().Name.Replace("DefMD", "").ToLower();
+            string originalName = entity.Name;
+            entity.Name = $"{typeName}_{count++}";
+
+            Console.WriteLine($"{originalName} -> {entity.Name}");
+        }
+
+        public static void RefactorAssembly(ModuleDefMD module)
+        {
+            int typeCount = 1;
+            int methodCount = 1;
+            int fieldCount = 1;
+            int eventCount = 1;
+            int nestedTypeCount = 1;
+
+            foreach (var typeDef in module.GetTypes())
+            {
+                RenameEntity(typeDef, ref typeCount);
+
+                foreach (var method in typeDef.Methods)
+                {
+                    RenameEntity(method, ref methodCount);
+
+                    int parameterCount = 1;
+                    foreach (var parameter in method.ParamDefs)
+                    {
+                        ProcessParameter(parameter, ref parameterCount);
+                    }
+                }
+
+                foreach (var field in typeDef.Fields)
+                {
+                    RenameEntity(field, ref fieldCount);
+                }
+
+                foreach (var eventDef in typeDef.Events)
+                {
+                    RenameEntity(eventDef, ref eventCount);
+                }
+
+                foreach (var nestedType in typeDef.NestedTypes)
+                {
+                    RenameEntity(nestedType, ref nestedTypeCount);
+                }
+            }
+        }
+    }
+}
